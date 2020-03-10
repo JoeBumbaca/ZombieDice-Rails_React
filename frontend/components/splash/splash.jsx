ad7b7266823c9210331469892517d2dd3c6d74ea
joe bumbaca <joebumbaca@gmail.com>
2020-03-09 17:58:28 -0700

    Prep for Heroku

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -16,9 +16,20 @@ const Splash = (props) => {
         </div>
       </div>
       <div className='splash-body'>
-        <div className='body-left'>Test1</div>
+        <div className='body-left'>
+          Eat brains. Don't get shotgunned. You are a zombie. You want braaains.
+          More brains than any of your zombie buddies. Zombie Dice is fun for
+          any zombie fan (or the whole zombie family). The 13 custom dice are
+          your victims.
+        </div>
         <img className='zombie' src={window.images.zombie} alt='zombie' />
-        <div className='body-right'>Test2</div>
+        <div className='body-right'>
+          Push your luck to eat their brains, but stop before the shotgun blasts
+          end your turn! It's a great quick game. Play it at lunch
+          (braaiiiiiins!) or while you're in line for a zombie movie . . . ! Two
+          or more can play. Each game takes 10 to 20 minutes, and can be taught
+          in a single round.
+        </div>
       </div>
     </div>
   );
