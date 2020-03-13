13a541ad69906e51e29ac650eebabcc22a2c09cd
joe bumbaca <joebumbaca@gmail.com>
2020-03-13 11:34:30 -0700

    Leaderboard work

--- frontend/components/leaderboard/leaderboard.jsx
+++ frontend/components/leaderboard/leaderboard.jsx
@@ -3,9 +3,17 @@ import React from 'react';
 const LeaderBoard = (props) => {
   return (
     <div className='leaderboard-container'>
-      I am the Leader Board
+      <h1 className='leaderboard-header'>Leader Board</h1>
+      <section className='leaderboard'>
+          <section className='fastest-win'>
+            <h1>Fastest Wins</h1>
+          </section>
+          <section className='most-brains'>
+            <h1>Most Brains in 1 Round</h1>
+          </section>
+      </section>
     </div>
-  )
+  );
 };
 
 export default LeaderBoard;
