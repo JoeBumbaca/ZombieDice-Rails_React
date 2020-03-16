8cc059d3d34471917fd7f8da57b14ff45c2dbaee
joe bumbaca <joebumbaca@gmail.com>
2020-03-16 10:05:38 -0700

    Start CSS Grid for leaderboard"

--- frontend/components/leaderboard/leaderboard.jsx
+++ frontend/components/leaderboard/leaderboard.jsx
@@ -7,6 +7,9 @@ const LeaderBoard = (props) => {
       <section className='leaderboard'>
           <section className='fastest-win'>
             <h1>Fastest Wins</h1>
+            <div className='fastest-win-grid'>
+              
+            </div>
           </section>
           <section className='most-brains'>
             <h1>Most Brains in 1 Round</h1>
