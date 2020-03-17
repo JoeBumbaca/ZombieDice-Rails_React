a5f7ae964d0699aa5a10ccd0983ce8a532051a87
joe bumbaca <joebumbaca@gmail.com>
2020-03-17 11:59:44 -0700

    Leaderboard Stuff

--- frontend/components/leaderboard/leaderboard.jsx
+++ frontend/components/leaderboard/leaderboard.jsx
@@ -8,11 +8,16 @@ const LeaderBoard = (props) => {
           <section className='fastest-win'>
             <h1>Fastest Wins</h1>
             <div className='fastest-win-grid'>
-              
+              <section className='grid-header'>Player</section>
+              <section className='grid-header'># of Rounds</section>
             </div>
           </section>
           <section className='most-brains'>
             <h1>Most Brains in 1 Round</h1>
+            <div className='fastest-win-grid'>
+              <section className='grid-header'>Player</section>
+              <section className='grid-header'># of Brains</section>
+            </div>
           </section>
       </section>
     </div>
